b19330032e2de6efbb563272ee9d3c05a2015877
bertino2 <berolbertindjomo@gmail.com>
2025-08-07 16:30:10 +0200

    arrangement du docker

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/api/hello', (req, res) => {
-  res.json({ message: 'Hello from backend!' });
+  res.json({ message: 'Helloooo from backend!' });
 });
 
 app.listen(PORT, () => {
